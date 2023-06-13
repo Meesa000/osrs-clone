fbb674e3300df8aaf778c10fd5d0558582387ec0
Meesa000 <asim_99@hotmail.co.uk>
2023-06-13 15:02:27 +0100

    add health and xp bar

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 
 // This was made in order to test and practice the basics of javascript
 
+const xpSpan = document.getElementById("xpSpan");
+const healthSpan = document.getElementById("healthSpan");
+
+const xpValue = 0;
+const healthValue = 99;
+
+xpSpan.textContent += xpValue;
+healthSpan.textContent += healthValue;
